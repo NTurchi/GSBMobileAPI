edfe5a92db019817f62ae209a4895a9983bdce8c
remi0411 <remi.lardier@epsi.fr>
2017-04-10 21:56:45 +0200

    Clear failed migrations

---
+++ APIGSB/Migrations/20170410195619_1.0-Migration.Designer.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using APIGSB.Models;
+
+namespace APIGSB.Migrations
+{
+    [DbContext(typeof(ApigsbDbContext))]
+    [Migration("20170410195619_1.0-Migration")]
+    partial class _10Migration
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.0-rtm-22752")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("APIGSB.Models.Excipient", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Libelle");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Excipient");
+                });
+
+            modelBuilder.Entity("APIGSB.Models.Famille", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Nom");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Famille");
+                });
+
+            modelBuilder.Entity("APIGSB.Models.Medecin", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Adresse");
+
+                    b.Property<int>("CodePostal");
+
+                    b.Property<string>("Email");
+
+                    b.Property<string>("HoraireVisite");
+
+                    b.Property<string>("ImgUrl");
+
+                    b.Property<double>("Latitude");
+
+                    b.Property<double>("Longitude");
+
+                    b.Property<string>("Nom");
+
+                    b.Property<string>("Prenom");
+
+                    b.Property<string>("Telephone");
+
+                    b.Property<int?>("VilleId");
+
+                    b.Property<int?>("VisiteurId");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("VilleId");
+
+                    b.HasIndex("VisiteurId");
+
+                    b.ToTable("Medecin");
+                });
+
+            modelBuilder.Entity("APIGSB.Models.Medicament", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Administration");
+
+                    b.Property<int>("Commande");
+
+                    b.Property<int?>("FamilleId");
+
+                    b.Property<string>("ImgUrl");
+
+                    b.Property<string>("Indications");
+
+                    b.Property<string>("Nom");
+
+                    b.Property<string>("Principe");
+
+                    b.Property<decimal>("Prix");
+
+                    b.Property<int>("Quantite");
+
+                    b.Property<string>("Status");
+
+                    b.Property<int>("Stock");
+
+                    b.Property<decimal>("TauxRemboursement");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("FamilleId");
+
+                    b.ToTable("Medicament");
+                });
+
+            modelBuilder.Entity("APIGSB.Models.MedicamentExcipient", b =>
+                {
+                    b.Property<int>("MedicamentId");
+
+                    b.Property<int>("ExcipientId");
+
+                    b.HasKey("MedicamentId", "ExcipientId");
+
+                    b.HasIndex("ExcipientId");
+
+                    b.ToTable("MedicamentExcipient");
+                });
+
+            modelBuilder.Entity("APIGSB.Models.MedicamentPathologie", b =>
+                {
+                    b.Property<int>("MedicamentId");
+
+                    b.Property<int>("PathologieId");
+
+                    b.HasKey("MedicamentId", "PathologieId");
+
+                    b.HasIndex("PathologieId");
+
+                    b.ToTable("MedicamentPathologie");
+                });
+
+            modelBuilder.Entity("APIGSB.Models.Pathologie", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Libelle");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Pathologie");
+                });
+
+            modelBuilder.Entity("APIGSB.Models.Ville", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("DepartementId");
+
+                    b.Property<string>("Nom");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Ville");
+                });
+
+            modelBuilder.Entity("APIGSB.Models.Visiteur", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Matricule");
+
+                    b.Property<string>("Nom");
+
+                    b.Property<string>("Prenom");
+
+                    b.Property<string>("Telephone");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Visiteur");
+                });
+
+            modelBuilder.Entity("APIGSB.Models.Medecin", b =>
+                {
+                    b.HasOne("APIGSB.Models.Ville", "Ville")
+                        .WithMany("Medecins")
+                        .HasForeignKey("VilleId");
+
+                    b.HasOne("APIGSB.Models.Visiteur", "Visiteur")
+                        .WithMany("Medecins")
+                        .HasForeignKey("VisiteurId");
+                });
+
+            modelBuilder.Entity("APIGSB.Models.Medicament", b =>
+                {
+                    b.HasOne("APIGSB.Models.Famille", "Famille")
+                        .WithMany("Medicaments")
+                        .HasForeignKey("FamilleId");
+                });
+
+            modelBuilder.Entity("APIGSB.Models.MedicamentExcipient", b =>
+                {
+                    b.HasOne("APIGSB.Models.Excipient", "Excipient")
+                        .WithMany("MedicamentExcipients")
+                        .HasForeignKey("ExcipientId")
+                        .OnDelete(DeleteBehavior.Cascade);
+
+                    b.HasOne("APIGSB.Models.Medicament", "Medicament")
+                        .WithMany("MedicamentExcipients")
+                        .HasForeignKey("MedicamentId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+
+            modelBuilder.Entity("APIGSB.Models.MedicamentPathologie", b =>
+                {
+                    b.HasOne("APIGSB.Models.Medicament", "Medicament")
+                        .WithMany("MedicamentPathologies")
+                        .HasForeignKey("MedicamentId")
+                        .OnDelete(DeleteBehavior.Cascade);
+
+                    b.HasOne("APIGSB.Models.Pathologie", "Pathologie")
+                        .WithMany("MedicamentPathologies")
+                        .HasForeignKey("PathologieId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
---
+++ APIGSB/Migrations/20170410195619_1.0-Migration.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace APIGSB.Migrations
+{
+    public partial class _10Migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Excipient",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Libelle = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Excipient", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Famille",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nom = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Famille", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pathologie",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Libelle = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pathologie", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ville",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    DepartementId = table.Column<int>(nullable: false),
+                    Nom = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ville", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Visiteur",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Matricule = table.Column<string>(nullable: true),
+                    Nom = table.Column<string>(nullable: true),
+                    Prenom = table.Column<string>(nullable: true),
+                    Telephone = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Visiteur", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Medicament",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Administration = table.Column<string>(nullable: true),
+                    Commande = table.Column<int>(nullable: false),
+                    FamilleId = table.Column<int>(nullable: true),
+                    ImgUrl = table.Column<string>(nullable: true),
+                    Indications = table.Column<string>(nullable: true),
+                    Nom = table.Column<string>(nullable: true),
+                    Principe = table.Column<string>(nullable: true),
+                    Prix = table.Column<decimal>(nullable: false),
+                    Quantite = table.Column<int>(nullable: false),
+                    Status = table.Column<string>(nullable: true),
+                    Stock = table.Column<int>(nullable: false),
+                    TauxRemboursement = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Medicament", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Medicament_Famille_FamilleId",
+                        column: x => x.FamilleId,
+                        principalTable: "Famille",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Medecin",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Adresse = table.Column<string>(nullable: true),
+                    CodePostal = table.Column<int>(nullable: false),
+                    Email = table.Column<string>(nullable: true),
+                    HoraireVisite = table.Column<string>(nullable: true),
+                    ImgUrl = table.Column<string>(nullable: true),
+                    Latitude = table.Column<double>(nullable: false),
+                    Longitude = table.Column<double>(nullable: false),
+                    Nom = table.Column<string>(nullable: true),
+                    Prenom = table.Column<string>(nullable: true),
+                    Telephone = table.Column<string>(nullable: true),
+                    VilleId = table.Column<int>(nullable: true),
+                    VisiteurId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Medecin", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Medecin_Ville_VilleId",
+                        column: x => x.VilleId,
+                        principalTable: "Ville",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Medecin_Visiteur_VisiteurId",
+                        column: x => x.VisiteurId,
+                        principalTable: "Visiteur",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MedicamentExcipient",
+                columns: table => new
+                {
+                    MedicamentId = table.Column<int>(nullable: false),
+                    ExcipientId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MedicamentExcipient", x => new { x.MedicamentId, x.ExcipientId });
+                    table.ForeignKey(
+                        name: "FK_MedicamentExcipient_Excipient_ExcipientId",
+                        column: x => x.ExcipientId,
+                        principalTable: "Excipient",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MedicamentExcipient_Medicament_MedicamentId",
+                        column: x => x.MedicamentId,
+                        principalTable: "Medicament",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MedicamentPathologie",
+                columns: table => new
+                {
+                    MedicamentId = table.Column<int>(nullable: false),
+                    PathologieId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MedicamentPathologie", x => new { x.MedicamentId, x.PathologieId });
+                    table.ForeignKey(
+                        name: "FK_MedicamentPathologie_Medicament_MedicamentId",
+                        column: x => x.MedicamentId,
+                        principalTable: "Medicament",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MedicamentPathologie_Pathologie_PathologieId",
+                        column: x => x.PathologieId,
+                        principalTable: "Pathologie",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Medecin_VilleId",
+                table: "Medecin",
+                column: "VilleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Medecin_VisiteurId",
+                table: "Medecin",
+                column: "VisiteurId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Medicament_FamilleId",
+                table: "Medicament",
+                column: "FamilleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MedicamentExcipient_ExcipientId",
+                table: "MedicamentExcipient",
+                column: "ExcipientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MedicamentPathologie_PathologieId",
+                table: "MedicamentPathologie",
+                column: "PathologieId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Medecin");
+
+            migrationBuilder.DropTable(
+                name: "MedicamentExcipient");
+
+            migrationBuilder.DropTable(
+                name: "MedicamentPathologie");
+
+            migrationBuilder.DropTable(
+                name: "Ville");
+
+            migrationBuilder.DropTable(
+                name: "Visiteur");
+
+            migrationBuilder.DropTable(
+                name: "Excipient");
+
+            migrationBuilder.DropTable(
+                name: "Medicament");
+
+            migrationBuilder.DropTable(
+                name: "Pathologie");
+
+            migrationBuilder.DropTable(
+                name: "Famille");
+        }
+    }
+}

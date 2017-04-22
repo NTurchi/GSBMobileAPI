fb63ee303a6c64a236f01300e1282fd933da8c55
remi0411 <remi.lardier@epsi.fr>
2017-04-22 20:04:02 +0200

    Fix request medicament

--- APIGSB/Models/Repository/MedicamentRepository.cs
+++ APIGSB/Models/Repository/MedicamentRepository.cs
@@ -32,14 +32,29 @@ namespace APIGSB.Models.Repository
 		/// <returns>Voir <see cref="IMedicamentRepository"/></returns>
 		public IEnumerable<Medicament> GetAll()
 		{
-			return _context.Medicament
-						       .Include(m => m.Famille).AsNoTracking()
-				       .Include(m => m.MedicamentPathologies)
-					       .ThenInclude(mp => mp.Pathologie)
-				       .Include(m => m.MedicamentExcipients)
-					       .ThenInclude(me => me.Excipient)
-                        .OrderBy(m=>m.Nom)
-				       .ToList();
+            return _context.Medicament
+                .Select(m => new Medicament
+                {
+                    Nom = m.Nom,
+                    Id = m.Id,
+                    Famille = new Famille()
+                    {
+                      Nom  = m.Famille.Nom
+                    },
+                    MedicamentPathologies = new List<MedicamentPathologie>(m.MedicamentPathologies),
+                    ImgUrl = m.ImgUrl,
+                    MedicamentExcipients = new List<MedicamentExcipient>(m.MedicamentExcipients),
+                    Administration = m.Administration,
+                    Commande = m.Commande,
+                    Indications = m.Indications,
+                    Principe = m.Principe,
+                    Prix = m.Prix,
+                    Quantite = m.Quantite,
+                    Status = m.Status,
+                    Stock = m.Stock,
+                    TauxRemboursement= m.TauxRemboursement
+                })
+                .ToList();
 		}
 
 		/// <summary>
